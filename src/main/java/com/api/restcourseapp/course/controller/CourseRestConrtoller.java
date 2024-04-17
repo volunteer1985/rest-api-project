1e354d7d1e208aa4d4cfe18d8fe459e442d5718e
Ivan Chornobai <ichornobai@yahoo.com>
2024-04-17 19:40:50 +0300

    Implementation finish for h2-db

--- src/main/java/com/api/restcourseapp/course/controller/CourseRestConrtoller.java
+++ src/main/java/com/api/restcourseapp/course/controller/CourseRestConrtoller.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,5 +39,18 @@ public class CourseRestConrtoller {
 	public void createCourse(@RequestBody Course course) {
 		repository.save(course);
 	}
-
+	
+	@PutMapping("courses/{id}")
+	public void updateCourseById(@PathVariable long id, @RequestBody Course course) {
+		repository.save(course);
+	}
+	
+	@DeleteMapping("courses/{id}")
+	public void deleteCourseById(@PathVariable long id) {
+		Optional<Course> courseToDelete = repository.findById(id);
+		if(courseToDelete.isEmpty()) {
+			throw new RuntimeException("No Course present with id=" + id + "\nNothing to delete");
+		}
+		repository.deleteById(id);
+	}
 }
